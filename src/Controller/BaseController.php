5dacc13e3967076c64abe0f6aa696a6b5360fa39
Jacob <yakoff.swidersky@gmail.com>
2024-07-25 20:31:53 +0300

    Service logic

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -2,17 +2,68 @@
 
 namespace App\Controller;
 
+use App\Service\ForecastService;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 class BaseController extends AbstractController
 {
+
+    private ForecastService $service;
+
+    public function __construct(ForecastService $forecastService)
+    {
+        $this->service = $forecastService;
+    }
+
     #[Route('/', name: 'app_index')]
     public function index(): JsonResponse
     {
         return $this->json([
             "Weather data service"
         ]);
+    }
+
+    #[Route("/forecast/now", name: "now", methods:"GET")]
+    public function now(): JsonResponse
+    {
+        try{
+            $result = $this->service->getActualNow();
+        } catch (Exception $e) {
+            dd($e->getMessage());
+            $result = "false";
+        }
+
+        return $this->json($result);
+    }
+
+    #[Route("/forecast", name: "get", methods:"GET")]
+    public function get(Request $request): JsonResponse
+    {
+        try{
+            $days = $request->get("days") ?? 7;
+            $result = $this->service->getData($days);
+        } catch (Exception $e) {
+            dd($e->getMessage());
+            $result = "false";
+        }
+
+        return $this->json($result);
+    }
+
+    #[Route("/forecast", name: "update", methods:"PATCH")]
+    public function update(): JsonResponse
+    {
+        try{
+            $this->service->updateData();
+            $result = "true";
+        } catch (Exception $e) {
+            $result = "false";
+        }
+
+        return $this->json($result);
     }
 }
---
+++ src/Service/ForecastService.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Forecast;
+use App\Parser\Factory\ParserFactory;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
+class ForecastService
+{
+
+    private const WEATHER_SITES = [
+        "WorldWeather",
+        "Gismeteo",
+        "Gidromet"
+    ];
+
+    private EntityManagerInterface $em;
+    private ServiceEntityRepository $repo;
+
+    public function __construct(EntityManagerInterface $entityManagerInterface)
+    {
+        $this->em = $entityManagerInterface;
+        $this->repo = $this->em->getRepository(Forecast::class);
+    }
+
+    public function getActualNow(): array
+    {
+        $result = [];
+
+        $now = date("Y-m-d H:i:s");
+        foreach(self::WEATHER_SITES as $site) {
+            $parser = ParserFactory::getParser($site);
+            if($parser !== false) {
+                $parsed = $parser->getNow();
+                $parsed['source'] = $site;
+                $parsed['date'] = $now;
+                $data[] = $parsed;
+            }
+        }
+
+        $result = $this->processData($data);
+        $result['average'] = $this->getAverage($data);
+
+        return $result ?? [];
+    }
+
+    public function getData(int $days = 7): array
+    {
+        $days = $days < 7 ? $days : 7;
+        $data = $this->repo->findByDays($days);
+        $result = [];
+
+        $result = $this->processData($data);
+        $result['average'] = $this->getAverage($data);
+        unset($data);
+
+        return $result ?? [];
+    }
+
+    public function updateData(): void
+    {
+        $data = [];
+        
+        foreach(self::WEATHER_SITES as $site) {
+            $parser = ParserFactory::getParser($site);
+            if($parser !== false) $data[$site] = $parser->getWeek();
+        }
+        
+        foreach($data as $source => $forecast) {
+            $this->repo->clearData($source);
+            $this->repo->saveFromArray($source,$forecast);
+        }
+    }
+    
+    private function processData(array $data): array
+    {
+        $result = [];
+
+        foreach($data as $item) {
+            $source = $item['source'];
+            unset($item['source']);
+            $item['date'] = is_string($item['date']) ? $item['date'] : $item['date']->format("Y-m-d H:i:s");;
+
+            $result[$source][] = $item;
+        }
+
+        return $result;
+    }
+
+    private function getAverage(array $data): array
+    {
+        $result = [];
+
+        foreach($data as $item) {
+            $date = is_string($item['date']) ? $item['date'] : $item['date']->format("Y-m-d H:i:s");
+            $result[$date]['date'] = $date;
+            if(!empty($item['weather'])) $result[$date]['weather'][] = $item['weather'];
+            if(!empty($item['temp'])) $result[$date]['temp'][] = $item['temp'];
+            if(!empty($item['wind'])) $result[$date]['wind'][] = $item['wind'];
+        }
+
+        foreach($result as &$forecast) {
+            $forecast['weather'] = array_unique($forecast['weather']);
+            $forecast['temp'] = strval(floor(array_sum($forecast['temp'])/count($forecast['temp'])));
+            $forecast['wind'] = strval(floor(array_sum($forecast['wind'])/count($forecast['wind'])));
+            unset($forecast);
+        }
+        
+        return array_values($result ?? []);
+    }
+}
